13fbe52160fb848ccd3276f44eceee5f70522f79
JoaoVittorMenezes <130706236+JoaoVittorMenezes@users.noreply.github.com>
2023-11-26 00:35:50 -0300

    comiy

---
+++ jogo.py
@@ -0,0 +1,8 @@
+def menu():
+    print(" 1- Salas Disponíveis\n 2- Criar nova sala\n 3- chat geral")
+    opcao = int(input("Digite a opção que deseja: "))
+    return opcao
+
+def criar sala():
+    #continuação...
+menu()
