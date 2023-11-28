e9f23d8920610ea90eab894deb6fd7be8e34e384
joao-menezes1 <148274276+joao-menezes1@users.noreply.github.com>
2023-11-28 20:15:11 +0000

    #comit

--- dicionarios.py
+++ dicionarios.py
@@ -1,5 +1,6 @@
-dicionario = {
+Temas = {
     'frutas': ['banana', 'maça', 'manga', 'uva', 'pera', 'melão', 'melancia', 'mamão', 'limão', 'ameixa', 'laranja', 'tangerina'],
+    'objetos': ['computador', 'celular', 'quadro', 'janela', 'porta'], 
 }
 
 salas = {
--- jogo.py
+++ jogo.py
@@ -1,4 +1,24 @@
-import FilaSequencialCircularNumPy
+import dicionarios
+import random
 
-def  menu():
-    print("      MENU      \n1  - Salas Disponíveis\n2 - Criar Sala\n3 - Chat Geral")
+
+class jogo:
+    def  menu():
+        print("      MENU      \n1  - Iniciar jogo....")
+        input('Digite a sua opção: ')
+    
+    def mostrar_temas():
+        temas = dicionarios.Temas
+        for tema, array in temas.items():
+            if tema != 'None':
+                print(tema + '\n')
+
+    def buscar_palavra():
+        for k in range(len(dicionarios.Temas)):
+            print(dicionarios.Temas)
+
+
+print(jogo.mostrar_temas())
+
+# v = dicionarios.Temas
+# print(v['frutas'][1])
