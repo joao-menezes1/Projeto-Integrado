c357a1e80f730083055b75de1debdfcab6f3afce
joao-menezes1 <148274276+joao-menezes1@users.noreply.github.com>
2023-11-29 21:00:30 +0000

    #commit

--- dicionario_temas_palavras.py
+++ dicionario_temas_palavras.py
@@ -1,6 +1,6 @@
 Temas = {
     'frutas': ['banana', 'maça', 'manga', 'uva', 'pera', 'melão', 'melancia', 'mamão', 'limão', 'ameixa', 'laranja', 'tangerina'],
-    'objetos': ['computador', 'celular', 'quadro', 'janela', 'porta'], 
+    'objetos': ['computador', 'celular', 'quadro', 'janela', 'porta', 'monitor', 'teclado', 'mouse', 'tela', 'teclas', 'roupas', 'camisa'], 
 }
 
 salas = {
--- jogo.py
+++ jogo.py
@@ -1,24 +1,57 @@
-import dicionarios
+import dicionario_temas_palavras
 import random
 
 
 class jogo:
-    def  menu():
+    def  __menu(self):
         print("      MENU      \n1  - Iniciar jogo....")
         input('Digite a sua opção: ')
     
-    def mostrar_temas():
-        temas = dicionarios.Temas
+    def __palavra_usuario(self):
+        tema = self.__mostrar_temas()
+        palavra = self.__buscar_palavra(tema)
+        print(palavra)
+
+
+    def __mostrar_temas(self):
+        temas = dicionario_temas_palavras.Temas
+        print("      TEMAS      \n")
         for tema, array in temas.items():
             if tema != 'None':
-                print(tema + '\n')
+                print("- ",tema)
+        
+        Tema_escolhido = input("Digite o tema escolhido: ")
+        return Tema_escolhido
 
-    def buscar_palavra():
-        for k in range(len(dicionarios.Temas)):
-            print(dicionarios.Temas)
+
+    def __buscar_palavra(self, Tema_escolhido):
+        Palavras_tema = dicionario_temas_palavras.Temas.get(Tema_escolhido)
+        Palavra_aleatoria_escolhida = Palavras_tema[random.randint(0, 11)]
+
+        return Palavra_aleatoria_escolhida
+    
+    def letras(self, letra, array_palavra_jogo, palavra):
+        for let in array_palavra_jogo:
+            if letra != let:
+                array_palavra_jogo[let] = '_'
+        print(array_palavra_jogo)
+
+        
+    def __iniciar_jogo(self):
+        menu = self.__menu()
+        palavra = self.__palavra_usuario()
+        print("Palavra já escolhida")
+        array_letras_escolhidas = []
+        letra = input('Digite uma letra: ')
+        array_palavra_jogo = palavra.slipt()
+        self.letras(letra, array_palavra_jogo, palavra)
+        
 
 
-print(jogo.mostrar_temas())
 
-# v = dicionarios.Temas
-# print(v['frutas'][1])
+
+
+
+
+        
+
